22761227488fdb0ba7f676736f37bb9f685f3cef
Majeed <majeedk526@gmail.com>
2019-04-19 00:02:54 +0530

    completed pipleine and added requirements

--- access_niu/train.py
+++ access_niu/train.py
@@ -1,4 +1,14 @@
-from access_niu.classifiers import mobilenet_v2
 
+import yaml
+
+from access_niu.classifiers import mobilenet_v2
+from access_niu import data
+
+with open('sample/sample_config.yml') as f:
+    config = yaml.safe_load(f)
 
 model = mobilenet_v2.get_model()
+model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['acc'])
+train_generator = data.data_generator(config.get('data').get('train'),224,224)
+
+model.fit_generator(generator=train_generator, steps_per_epoch=888/32 ,epochs=10)
