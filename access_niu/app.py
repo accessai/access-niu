90c1573a5a06ebfa5037be4f2b30a632d6a6e6a7
Majeed <majeedk526@gmail.com>
2019-05-05 11:08:50 +0530

    resize image while parsing

--- access_niu/app.py
+++ access_niu/app.py
@@ -13,6 +13,13 @@ class NIUApp(object):
         self.model_dir = template.get("project").get("path")
         self.model, self.labels = load_model(self.model_dir)
         self.trainer = Trainer(template)
+        # TODO: Load it at project level
+        self.image_size = self._get_image_dimensions(template)
+
+    def _get_image_dimensions(self, template):
+        input_layer = template.get("pipeline")[0].get("input_layer")
+
+        return input_layer.get("image_width"), input_layer.get("image_height")
 
     def parse(self, data: FileStorage) -> dict:
         """Handles the parse request.
@@ -20,7 +27,8 @@ class NIUApp(object):
         :param data: Image file to be parsed.
         :return: dic of the parsed response
         """
-        img = np.expand_dims(np.array(Image.open(data)), axis=0)
+        img = Image.open(data).resize(self.image_size, Image.ANTIALIAS)
+        img = np.expand_dims(np.array(img), axis=0)
         pred_arr = parse(self.model, img)
         max_idx = np.argmax(pred_arr)
         return {self.labels.get(max_idx): np.float(pred_arr[0][max_idx])}
